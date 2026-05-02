d287e5a06311558562a8c62d68f6347681e4fbc7
shivangrana2006 <sr7169953@gmail.com>
2026-05-02 09:56:51 +0530

    Update API URL for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000" });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000"
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
